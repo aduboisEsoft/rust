c4ee3090293b081b42a4142201bb39e2d2d84ead
Arnaud <adubois@3bstudio.fr>
2023-04-04 07:43:12 +0200

    Configure service layer

---
+++ API/src/tweet.rs
@@ -0,0 +1,92 @@
+use actix_web::web::{Json,Path};
+use actix_web::HttpResponse;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::constants::APPLICATION_JSON;
+use crate::like::Like;
+use crate::response::Response;
+
+pub type Tweets = Response<Tweet>;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Tweet {
+    pub id: String,
+    pub created_at: DateTime<Utc>,
+    pub message: String,
+    pub likes: Vec<Like>,
+}
+
+impl Tweet {
+    pub fn new(message: String) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            created_at: Utc::now(),
+            message,
+            likes: vec![],
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TweetRequest {
+    pub message: Option<String>,
+}
+
+impl TweetRequest {
+    pub fn to_tweet(&self) -> Option<Tweet> {
+        match &self.message {
+            Some(message) => Some(Tweet::new(message.to_string())),
+            None => None,
+        }
+    }
+}
+
+#[post("/tweets")]
+pub async fn create(tweet_req: Json<TweetRequest>) -> HttpResponse {
+    HttpResponse::Created()
+        .content_type(APPLICATION_JSON)
+        .json(tweet_req.to_tweet())
+}
+
+#[get("/tweets/{id}")]
+pub async fn get(path: Path<(String,)>) -> HttpResponse {
+    let found_tweet: Option<Tweet> = None;
+
+    match found_tweet {
+        Some(tweet) => HttpResponse::Ok()
+            .content_type(APPLICATION_JSON)
+            .json(tweet),
+        None => HttpResponse::NoContent()
+            .content_type(APPLICATION_JSON)
+            .await
+            .unwrap(),
+    }
+}
+
+#[get("/tweets")]
+pub async fn list() -> HttpResponse {
+    let found_tweets: Option<Vec<Tweet>> = None;
+
+    match found_tweets {
+        Some(tweets) => HttpResponse::Ok()
+            .content_type(APPLICATION_JSON)
+            .json(tweets),
+        None => HttpResponse::NoContent()
+            .content_type(APPLICATION_JSON)
+            .await
+            .unwrap(),
+    }
+}
+/// delete a tweet by its id `/tweets/{id}`
+#[delete("/tweets/{id}")]
+pub async fn delete(path: Path<(String,)>) -> HttpResponse {
+    // TODO delete tweet by ID
+    // in any case return status 204
+
+    HttpResponse::NoContent()
+        .content_type(APPLICATION_JSON)
+        .await
+        .unwrap()
+}
